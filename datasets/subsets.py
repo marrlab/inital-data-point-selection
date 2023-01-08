962f9e9e6ff9c868e5216a4892b428929f79a5da
Miroslav Březík <mirek.brezik@gmail.com>
2023-01-08 21:34:28 +0100

    implemented subsetting based on badge active learning method

--- datasets/subsets.py
+++ datasets/subsets.py
@@ -5,16 +5,23 @@ import random
 import numpy as np
 from datasets.datasets import ImageDataset
 from typing import List
+from sklearn.cluster import kmeans_plusplus
+
 
 def get_by_indices(dataset: ImageDataset, indices: List[int]) -> ImageDataset:
     new_dataset = copy.deepcopy(dataset)
 
-    new_dataset.images_data['names'] = list(np.array(new_dataset.images_data['names'])[indices])
-    new_dataset.images_data['labels_text'] = list(np.array(new_dataset.images_data['labels_text'])[indices])
-    new_dataset.images_data['labels'] = list(np.array(new_dataset.images_data['labels'])[indices])
-    new_dataset.images_data['paths'] = list(np.array(new_dataset.images_data['paths'])[indices])
+    new_dataset.images_data['names'] = list(
+        np.array(new_dataset.images_data['names'])[indices])
+    new_dataset.images_data['labels_text'] = list(
+        np.array(new_dataset.images_data['labels_text'])[indices])
+    new_dataset.images_data['labels'] = list(
+        np.array(new_dataset.images_data['labels'])[indices])
+    new_dataset.images_data['paths'] = list(
+        np.array(new_dataset.images_data['paths'])[indices])
 
     return new_dataset
+
 
 def get_n_random(dataset: ImageDataset, n: int) -> ImageDataset:
     assert n <= len(dataset)
@@ -24,6 +31,7 @@ def get_n_random(dataset: ImageDataset, n: int) -> ImageDataset:
 
     return new_dataset
 
+
 def get_n_sorted_by_feature_func(dataset: ImageDataset, n: int, func, n_smallest=True) -> ImageDataset:
     assert dataset.features_path is not None
     assert n <= len(dataset)
@@ -31,8 +39,24 @@ def get_n_sorted_by_feature_func(dataset: ImageDataset, n: int, func, n_smallest
     feature_func_values = []
     for data_point in dataset:
         feature_func_values.append(func(data_point['feature']))
-        
+
     indices = np.argsort(feature_func_values)[:n]
+    new_dataset = get_by_indices(dataset, indices)
+
+    return new_dataset
+
+
+def get_n_kmeans_plus_plus(dataset: ImageDataset, n: int) -> ImageDataset:
+    assert dataset.features_path is not None
+    assert n <= len(dataset)
+
+    features = []
+    for name in dataset.images_data['names']:
+        features.append(dataset.features[name])
+
+    features = np.array(features)
+    centers, indices = kmeans_plusplus(features, n_clusters=n, random_state=0)
+
     new_dataset = get_by_indices(dataset, indices)
 
     return new_dataset
--- tasks/training.py
+++ tasks/training.py
@@ -7,40 +7,40 @@ from models.lightning_modules import ImageClassifierLightningModule
 from datasets.datasets import ImageDataset
 
 
-
-
 def train_image_classifier(model: torch.nn.Module, train_dataset: ImageDataset, val_dataset: ImageDataset):
     assert len(train_dataset.labels) == len(val_dataset.labels)
 
-    train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=wandb.config.batch_size, shuffle=True)
+    train_data_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=wandb.config.batch_size, shuffle=True)
     batches = int(np.ceil(len(train_dataset) / wandb.config.batch_size))
 
-    val_data_loader = torch.utils.data.DataLoader(val_dataset, batch_size=wandb.config.batch_size, shuffle=True)
+    val_data_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=wandb.config.batch_size, shuffle=True)
     wandb_logger = pl.loggers.WandbLogger()
 
     class LogPredictionSamplesCallback(pl.Callback):
         def on_validation_batch_end(
-            self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+                self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
             """Called when the validation batch ends."""
- 
+
             # `outputs` comes from `LightningModule.validation_step`
             # which corresponds to our model predictions in this case
-        
+
             # Let's log 20 sample image predictions from the first batch
             if batch_idx == 0:
                 n = 20
                 images = [img for img in batch['image'][:n]]
-                captions = [f'Ground Truth: {y_i} - Prediction: {y_pred}' 
-                    for y_i, y_pred in zip(batch['label'][:n], outputs[:n])]
-            
-            
+                captions = [f'Ground Truth: {y_i} - Prediction: {y_pred}'
+                            for y_i, y_pred in zip(batch['label'][:n], outputs[:n])]
+
                 # Option 1: log images with `WandbLogger.log_image`
                 wandb_logger.log_image(
-                    key='sample_images', 
-                    images=images, 
+                    key='validation_sample_images',
+                    images=images,
                     caption=captions)
 
-    lightning_model = ImageClassifierLightningModule(model, len(train_dataset.labels))
+    lightning_model = ImageClassifierLightningModule(
+        model, len(train_dataset.labels))
     wandb_logger.watch(lightning_model)
     trainer = pl.Trainer(
         max_epochs=wandb.config.epochs,
@@ -53,7 +53,6 @@ def train_image_classifier(model: torch.nn.Module, train_dataset: ImageDataset, 
 
     trainer.fit(
         lightning_model,
-        train_dataloaders=train_data_loader, 
+        train_dataloaders=train_data_loader,
         val_dataloaders=val_data_loader,
     )
-
