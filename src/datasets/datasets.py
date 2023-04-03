reindexing for correct metric computation implemented

src/datasets/datasets.py
@@ -46,7 +46,10 @@ class ImageDataset(torch.utils.data.Dataset):
         self.images_dir = None
         self.labels_text = []
         self.labels = []
+        self.classes = []
         self.labels_text_mapping = {}
+        self.label_to_class_mapping = {}
+        self.class_to_label_mapping = {}
 
         self.images_data = {
             'names': [],
@@ -66,6 +69,7 @@ class ImageDataset(torch.utils.data.Dataset):
     def __getitem__(self, i):
         data_point = {
             'label': self.images_data['labels'][i],
+            'class': self.label_to_class_mapping[self.images_data['labels'][i]],
             'name': self.images_data['names'][i],
         }
 
@@ -88,36 +92,31 @@ class ImageDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.images_data['names'])
 
-    # restructures labeling, removing those for which there are no data points
-    # e.g. possible lables [0, 1, 2, 3, 4], present labels [0, 3, 4], new labels [0, 1, 2]
-    def relabel(self):
-        labels_unique = set(self.images_data['labels'])
-        labels_to_delete = list(set(range(len(self.labels))) - labels_unique)
+    # restructures label to class assignment, removing those for which there are no data points
+    # e.g. possible lables [0, 1, 2, 3, 4], present labels [0, 3, 4], new classes [0, 1, 2]
+    def reassign_classes(self):
+        labels_remaining = sorted(list(set(self.images_data['labels'])))
+        labels_missing = list(set(self.labels) - set(labels_remaining))
 
-        self.labels_text = np.delete(self.labels_text, labels_to_delete)
-        self.labels = list(range(len(labels_unique)))
-        self.labels_text_mapping = {text: id for id, text in enumerate(self.labels_text)} 
+        self.classes = list(range(len(labels_remaining)))
 
-        for i in range(len(self)):
-            self.images_data['labels'][i] = self.labels_text_mapping[self.images_data['labels_text'][i]]
+        self.label_to_class_mapping = {
+            label: label_index
+            for label_index, label in enumerate(labels_remaining)
+        }
+        for label in labels_missing:
+            self.label_to_class_mapping[label] = None
 
-    # changes the labeling so that it corresponds to the source dataset
-    # removes data points with labels (text) that don't appear in the source dataset
-    def match_labels_and_filter(self, source_dataset):
-        self.labels_text = source_dataset.labels_text.copy()
-        self.labels = source_dataset.labels.copy()
-        self.labels_text_mapping = source_dataset.labels_text_mapping.copy()
+        self.class_to_label_mapping = {
+            _class: labels_remaining[_class]
+            for _class in self.classes
+        }
 
-        for i in reversed(range(len(self))):
-            if self.images_data['labels_text'][i] not in self.labels_text:
-                del self.images_data['names'][i]
-                del self.images_data['labels_text'][i]
-                del self.images_data['labels'][i]
-                del self.images_data['paths'][i]
-
-                continue
-
-            self.images_data['labels'][i] = self.labels_text_mapping[self.images_data['labels_text'][i]]
+    # changes the label to class assignment so that it corresponds to the source dataset
+    def match_classes_and_filter(self, source_dataset):
+        self.classes = source_dataset.classes.copy()
+        self.label_to_class_mapping = source_dataset.label_to_class_mapping.copy()
+        self.class_to_label_mapping = source_dataset.class_to_label_mapping.copy()
 
     def standard_scale_features(self):
         keys = list(self.features.keys())
@@ -137,6 +136,12 @@ class ImageDataset(torch.utils.data.Dataset):
         for i, key in enumerate(keys):
             self.features[key] = X_new[i]
 
+    def get_number_of_classes(self):
+        return len(self.classes)
+
+    def get_number_of_labels(self):
+        return len(self.labels)
+
 
 class ImageDatasetWithFolderStructure(ImageDataset):
     def __init__(
@@ -155,6 +160,14 @@ class ImageDatasetWithFolderStructure(ImageDataset):
         self.labels_text = sorted([f.name for f in os.scandir(self.images_dir) if f.is_dir()])
         self.labels = list(range(len(self.labels_text)))
         self.labels_text_mapping = {text: id for id, text in enumerate(self.labels_text)}
+        self.label_to_class_mapping = {
+            label: label
+            for label in self.labels
+        }
+        self.class_to_label_mapping = {
+            label: label
+            for label in self.labels
+        }
         
         for label_text in self.labels_text:
             label_dir = os.path.join(self.images_dir, label_text)

src/models/lightning_modules.py
@@ -6,7 +6,7 @@ import torchvision
 import lightning.pytorch as pl
 from torchmetrics.functional import accuracy, f1_score
 from sklearn.metrics import balanced_accuracy_score, matthews_corrcoef, cohen_kappa_score
-from src.utils.utils import flatten_tensor_dicts
+from src.utils.utils import flatten_tensor_dicts, map_tensor_values
 from collections import defaultdict
 from lightly.data import LightlyDataset, SimCLRCollateFunction, collate
 from lightly.models.modules.heads import SimCLRProjectionHead
@@ -14,17 +14,16 @@ from lightly.loss import NTXentLoss
 
 
 class ImageClassifierLightningModule(pl.LightningModule):
-    def __init__(self, model, num_classes, cfg, labels_text=None):
+    def __init__(self, model, num_labels, num_classes, label_to_class_mapping, class_to_label_mapping, cfg):
         super().__init__()
 
         self.cfg = cfg
         self.model = model
+        self.num_labels = num_labels
         self.num_classes = num_classes
+        self.label_to_class_mapping = label_to_class_mapping
+        self.class_to_label_mapping = class_to_label_mapping
         self.loss = torch.nn.CrossEntropyLoss()
-        self.labels_text = labels_text
-        if self.labels_text is None:
-            self.labels_text = list(
-                f'label {i}' for i in range(self.num_classes))
 
         self.step_outputs = {
             'train': [],
@@ -54,20 +53,21 @@ class ImageClassifierLightningModule(pl.LightningModule):
         outputs = self.step_outputs[step]
         outputs = flatten_tensor_dicts(outputs)
 
-        self.metrics_epoch_end[f'{step}_loss_epoch_end'].append(
-            torch.mean(outputs['loss']).detach().item())
+        if step == 'train':
+            self.metrics_epoch_end[f'{step}_loss_epoch_end'].append(
+                torch.mean(outputs['loss']).detach().item())
         self.metrics_epoch_end[f'{step}_accuracy_epoch_end'].append(accuracy(
-            outputs['preds'], outputs['labels'], task='multiclass', num_classes=self.num_classes).detach().item())
+            outputs['pred_labels'], outputs['labels'], task='multiclass', num_classes=self.num_labels).detach().item())
         self.metrics_epoch_end[f'{step}_f1_macro_epoch_end'].append(f1_score(
-            outputs['preds'], outputs['labels'], task='multiclass', num_classes=self.num_classes, average='macro').detach().item())
+            outputs['pred_labels'], outputs['labels'], task='multiclass', num_classes=self.num_labels, average='macro').detach().item())
         self.metrics_epoch_end[f'{step}_f1_micro_epoch_end'].append(f1_score(
-            outputs['preds'], outputs['labels'], task='multiclass', num_classes=self.num_classes, average='micro').detach().item())
-        self.metrics_epoch_end[f'{step}_balanced_accuracy'].append(
-            balanced_accuracy_score(outputs['labels'].cpu().numpy(), outputs['preds'].cpu().numpy()))
-        self.metrics_epoch_end[f'{step}_matthews_corrcoef'].append(
-            matthews_corrcoef(outputs['labels'].cpu().numpy(), outputs['preds'].cpu().numpy()))
-        self.metrics_epoch_end[f'{step}_cohen_kappa_score'].append(
-            cohen_kappa_score(outputs['labels'].cpu().numpy(), outputs['preds'].cpu().numpy()))
+            outputs['pred_labels'], outputs['labels'], task='multiclass', num_classes=self.num_labels, average='micro').detach().item())
+        self.metrics_epoch_end[f'{step}_balanced_accuracy_epoch_end'].append(
+            balanced_accuracy_score(outputs['labels'].cpu().numpy(), outputs['pred_labels'].cpu().numpy()))
+        self.metrics_epoch_end[f'{step}_matthews_corrcoef_epoch_end'].append(
+            matthews_corrcoef(outputs['labels'].cpu().numpy(), outputs['pred_labels'].cpu().numpy()))
+        self.metrics_epoch_end[f'{step}_cohen_kappa_score_epoch_end'].append(
+            cohen_kappa_score(outputs['labels'].cpu().numpy(), outputs['pred_labels'].cpu().numpy()))
 
         for key in self.metrics_epoch_end:
             self.log(key, self.metrics_epoch_end[key][-1])
@@ -79,7 +79,7 @@ class ImageClassifierLightningModule(pl.LightningModule):
     def _common_step(self, batch, batch_idx, step):
         assert step in ('train', 'val')
 
-        images, labels = batch['image'], batch['label']
+        images, classes, labels = batch['image'], batch['class'], batch['label']
 
         assert images.ndim == 4
 
@@ -87,19 +87,29 @@ class ImageClassifierLightningModule(pl.LightningModule):
         assert h % 32 == 0 and w % 32 == 0
 
         logits = self(images)
-        preds = torch.argmax(logits, dim=1)
+        pred_classes = torch.argmax(logits, dim=1)
 
-        loss = self.loss(logits, labels)
+        loss = None
+        if step == 'train':
+            loss = self.loss(logits, classes)
 
-        self.log(f'{step}_loss', loss)
-        self.log(f'{step}_accuracy', accuracy(preds, labels,
-                 task='multiclass', num_classes=self.num_classes))
-        self.log(f'{step}_f1_macro', f1_score(
-            preds, labels, task='multiclass', num_classes=self.num_classes, average='macro'))
-        self.log(f'{step}_f1_micro', f1_score(
-            preds, labels, task='multiclass', num_classes=self.num_classes, average='micro'))
+        # self.log(f'{step}_loss', loss)
+        # self.log(f'{step}_accuracy', accuracy(preds, labels,
+        #          task='multiclass', num_classes=self.num_classes))
+        # self.log(f'{step}_f1_macro', f1_score(
+        #     preds, labels, task='multiclass', num_classes=self.num_classes, average='macro'))
+        # self.log(f'{step}_f1_micro', f1_score(
+        #     preds, labels, task='multiclass', num_classes=self.num_classes, average='micro'))
 
-        output = {'loss': loss, 'preds': preds, 'labels': labels}
+        output = {
+            'pred_classes': pred_classes,
+            'pred_labels': map_tensor_values(pred_classes, self.class_to_label_mapping),
+            'classes': classes,
+            'labels': labels
+        }
+        if loss is not None:
+            output['loss'] = loss
+            
         self.step_outputs[step].append(output)
 
         return output

src/models/training.py
@@ -14,8 +14,8 @@ from omegaconf import DictConfig
 from hydra.utils import get_original_cwd
 
 def train_image_classifier(model: torch.nn.Module, train_dataset: ImageDataset, val_dataset: ImageDataset, cfg: DictConfig):
-    # TODO: revise
     assert len(train_dataset.labels) == len(val_dataset.labels)
+    assert len(train_dataset.classes) == len(val_dataset.classes)
 
     train_data_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=cfg.training.batch_size, shuffle=True)
@@ -38,7 +38,7 @@ def train_image_classifier(model: torch.nn.Module, train_dataset: ImageDataset, 
                 n = 16
                 images = list(batch['image'][:n])
                 captions = [f'Ground Truth: {y_i} - Prediction: {y_pred}'
-                            for y_i, y_pred in zip(batch['label'][:n], outputs['preds'][:n])]
+                            for y_i, y_pred in zip(batch['label'][:n], outputs['pred_labels'][:n])]
 
                 # Option 1: log images with `WandbLogger.log_image`
                 wandb_logger.log_image(
@@ -47,7 +47,13 @@ def train_image_classifier(model: torch.nn.Module, train_dataset: ImageDataset, 
                     caption=captions)
 
     lightning_model = ImageClassifierLightningModule(
-        model, len(train_dataset.labels), cfg, labels_text=train_dataset.labels_text)
+        model,
+        train_dataset.get_number_of_labels(),
+        train_dataset.get_number_of_classes(),
+        train_dataset.label_to_class_mapping.copy(),
+        train_dataset.class_to_label_mapping.copy(),
+        cfg
+    )
     wandb_logger.watch(lightning_model)
     trainer = pl.Trainer(
         max_epochs=cfg.training.epochs,
@@ -56,10 +62,10 @@ def train_image_classifier(model: torch.nn.Module, train_dataset: ImageDataset, 
         accelerator='gpu' if torch.cuda.is_available() else 'cpu',
         devices=1,
         callbacks=[
-            # TODO: commented for debugging
-            # LogPredictionSamplesCallback(),
-            ModelCheckpoint(mode='min', monitor='val_loss_epoch_end',
-                            save_top_k=3, filename='{epoch}-{step}-{val_loss_ssl:.2f}'),
+            # TODO: comment out for debugging
+            LogPredictionSamplesCallback(),
+            ModelCheckpoint(mode='max', monitor='val_f1_macro_epoch_end',
+                            save_top_k=2, filename='{epoch}-{step}-{val_loss_ssl:.2f}'),
             ModelCheckpoint(every_n_epochs=10),
             LearningRateMonitor('epoch'),
         ]